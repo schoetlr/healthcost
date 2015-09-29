e83407b4ee9b981de10456bfc75a01849f67b98c
Luke Schoettinger <lukeschoettinger@yahoo.com>
2015-09-28 22:11:32 -0400

    hack failed back to normal, should have made a branch

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.configure do
-
-config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
-
-end
